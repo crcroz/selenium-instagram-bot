16c857cde0c47684af9d6452d3d0444a9242b679
crcroz <73910868+crcroz@users.noreply.github.com>
2020-12-15 11:44:33 -0800

    Add files via upload

---
+++ follow_user_followers.py
@@ -0,0 +1,72 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import time
+
+
+def follow_page_followers(pagename, n):
+	#OPENS INSTAGRAM
+	binary = r'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+	options = Options()
+	options.binary = binary
+	browser = webdriver.Firefox(firefox_options=options, executable_path="your path")
+	browser.implicitly_wait(5)
+	browser.get('http://instagram.com/')
+
+	#LOGS IN AND GETS YOU PAST THE 'SAVE PASSWORD' AND 'TURN ON NOTIFICATIONS' PROMPTS
+	username_input = browser.find_element_by_css_selector("input[name='username']")
+	password_input = browser.find_element_by_css_selector("input[name='password']")
+	username_input.send_keys("your username")
+	password_input.send_keys("your password")
+	login_button = browser.find_element_by_xpath("//button[@type='submit']")
+	login_button.click()
+	time.sleep(3)
+	not_now_button = browser.find_element_by_class_name("sqdOP.yWX7d.y3zKF")
+	not_now_button.click()
+	time.sleep(2)
+	no_notifications = browser.find_element_by_class_name("aOOlW.HoLwm ")
+	no_notifications.click()
+	time.sleep(2)
+
+	#SEARCHS FOR YOUR TARGET PAGE IN THE SEARCH BAR AND SELECTS THE FIRST OPTION
+	#MAKE SURE TO INCLUDE THE FULL PAGENAME SO IT IS THE FIRST OPTION
+	search = browser.find_element_by_class_name('XTCLo.x3qfX')
+	search.send_keys(pagename)
+	time.sleep(1)
+	page = browser.find_element_by_class_name('z556c')
+	page.click()
+	time.sleep(2)
+
+	#CLICKS ON THE FOLLOWERS BUTTON TO DISPLAY THE FOLLOWERS OF THE PAGE
+	page_followers = browser.find_element_by_xpath(f"//a[@href = '/{pagename}/followers/']")
+	page_followers.click()
+
+	
+	count = 0
+	while count<=n:
+		#ONCE YOU FOLLOW N NUMBER OF PEOPLE IT CLOSES
+		if count == n:
+			browser.quit()
+		else:
+			try:
+				#CLICKS FOLLOW ON A USER YOU ARE NOT FOLLOWING
+				follow = browser.find_element_by_class_name('sqdOP.L3NKy.y3zKF')
+				follow.click()
+				count+=1
+				print(f'followed someone {count}')
+			except: 
+				#SCROLLS SO UNTIL IT SEES A FOLLOW BUTTON
+				fBody  = browser.find_element_by_xpath("//div[@class='isgrP']")
+				scroll = 0
+				print('scrolling')
+				while scroll < 2: # scroll 2 times
+				    browser.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', fBody)
+				    time.sleep(2)
+				    scroll += 1
+
+while True:		    
+	#THIS HERE FOLLOWS 20 PEOPLE FROM EACH PAGE EVERY HOUR
+	#THIS IS A SAFE LIMIT SO YOU ARE NOT LIMITED BY INSTAGRAM
+	x = follow_page_followers(pagename = 'name of page you wish to follow their users', n = 20)
+	y = follow_page_followers(pagename = 'name of page you wish to follow their users', n = 20)
+	z = follow_page_followers(pagename = 'name of page you wish to follow their users', n = 20)
+	time.sleep(3600)
